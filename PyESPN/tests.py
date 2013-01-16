7ea147a24d89bd716b7b4b457b958cfe4d04affe
Christopher Brown <chrbrown@sas.upenn.edu>
2013-01-15 20:25:21 -0500

    first test written

--- PyESPN/tests.py
+++ PyESPN/tests.py
@@ -7,8 +7,12 @@ import unittest
 
 class TestPyESPN(unittest.TestCase):
     """Test basic functions in PyESPN class"""
-    def setUp():
-        self.search = PyESPN(main_url, API_KEY)
+    def setUp(self):
+        self.search = PyESPN(API_KEY)
 
     def test_url(self):
+        self.search.sports()
+        self.assertTrue(len(self.search.sport_list.items()) > 1)
 
+if __name__ == '__main__':
+    unittest.main()
